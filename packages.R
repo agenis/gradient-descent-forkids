fcde6d8811874d6b588fd5ddbdd3307d962a4292
agenis <marc.agenis@gmail.com>
2018-06-06 20:11:08 +0000

    final script 3/3

---
+++ packages.R
@@ -0,0 +1,15 @@
+
+#########################################################################
+# ADAPTATION DE LA DESCENTE DE GRADIENT A UN JEU DE RECHERCHE POUR ENFANT
+#########################################################################
+
+####################
+# PACKAGES A CHARGER
+
+library(dplyr)
+library(ggplot2)
+library(distr)
+library(zoo)
+library(png)
+library(grid)
+library(triangle)
